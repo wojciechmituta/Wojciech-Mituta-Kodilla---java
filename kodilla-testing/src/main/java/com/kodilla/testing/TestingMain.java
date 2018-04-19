ded17217c8c819ca49da912a6743c6790389dc8c
“wojciechmituta <38225947+wojciechmituta@users.noreply.github.com>
2018-04-19 20:22:55 +0200

    zadanie 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        Calculator calculator = new Calculator ();
+        int result = calculator.addAToB(2, 5);
+        if (result == 7) {
+            System.out.println("test ok!");
+        }else{
+            System.out.println("test failed!");
+        }
+
+        Calculator calculator1 = new Calculator ();
+        int result1 = calculator1.addAToB(2, 5);
+        if (result1 == 7) {
+            System.out.println("test ok!");
+        }else{
+            System.out.println("test failed!");
+        }
     }
-}
+
+    }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    private int a;
+    private int b;
+
+    public int substractAFromB(int a, int b){
+        return a - b;
+    }
+
+    public int addAToB(int a, int b){
+        return a + b;
+    }
+}
