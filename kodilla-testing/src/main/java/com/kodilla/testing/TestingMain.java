20a4ce08d5d7e09d9d8fb8c394eae1528136e1ea
“wojciechmituta <38225947+wojciechmituta@users.noreply.github.com>
2018-04-20 16:38:41 +0200

    zadanie 6.2.1

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,20 +5,15 @@ import com.kodilla.testing.calculator.Calculator;
 public class TestingMain {
     public static void main(String[] args){
         Calculator calculator = new Calculator ();
-        int result = calculator.addAToB(2, 5);
-        if (result == 7) {
+        int sumResult = calculator.addAToB(2, 5);
+        int subResult = calculator.substractAFromB(8, 5);
+
+
+        if ((sumResult == 7) && (subResult == 3)) {
             System.out.println("test ok!");
-        }else{
+        }else {
             System.out.println("test failed!");
         }
 
-        Calculator calculator1 = new Calculator ();
-        int result1 = calculator1.addAToB(2, 5);
-        if (result1 == 7) {
-            System.out.println("test ok!");
-        }else{
-            System.out.println("test failed!");
-        }
     }
-
-    }
+}
