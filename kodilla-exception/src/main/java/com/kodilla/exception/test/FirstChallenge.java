0d79593a51ab555d8f004b6b03e239c353d788d9
“wojciechmituta <38225947+wojciechmituta@users.noreply.github.com>
2018-05-03 19:46:13 +0200

    Zadanie 8.2.1

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -3,34 +3,22 @@ package com.kodilla.exception.test;
 public class FirstChallenge {
 
     public double divide(double a, double b) throws ArithmeticException {
-
-        try {
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
-            return a / b;
-
-        } catch (ArithmeticException e) {
-
-            System.out.println("Sorry dividing by 0 is impossible");
-
-        } finally {
-
-            System.out.println("Is Done");
-
+        if (b == 0) {
+            throw new ArithmeticException();
         }
-
-        return 0;
+        return a / b;
     }
 
     public static void main(String[] args) {
 
         FirstChallenge firstChallenge = new FirstChallenge();
-
-        double result = firstChallenge.divide(3, 0
-        );
-
-        System.out.println(result);
-
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Divide by 0 is impossible!");
+        } finally {
+            System.out.println("Is done!");
+        }
     }
 }
